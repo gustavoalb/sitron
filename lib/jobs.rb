45ea62f2bc6a34750fb9c5febe0982b0ddf2368a
Gustavo Albuquerque <alucard.ap@gmail.com>
2014-07-23 13:24:31 -0300

    Clockwork

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -1,5 +1,5 @@
-require File.expand_path('../../config/boot',        __FILE__)
-require File.expand_path('../../config/environment', __FILE__)
+require './config/boot'
+require './config/environment'
 
 class Fila
   def deliver
