8380cbb190860890189b5925df75d759723c3119
Gustavo <gustavo@SKYNET-LAPTOP.(none)>
2014-07-23 18:55:15 -0300

    libs

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -1,5 +1,8 @@
-require './config/boot'
-require './config/environment'
+app_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+$LOAD_PATH.unshift(app_path) unless $LOAD_PATH.include?(app_path)
+require 'clockwork'
+require 'config/boot'
+require 'config/environment'
 
 class Fila
   def deliver
