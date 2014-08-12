b6a59b166e6979b40de3f9947a041b53c254d6b1
Gustavo Albuquerque <alucard.ap@gmail.com>
2014-08-12 16:39:10 -0300

    Correção de Layouts e Adição de Requisição Especial

--- app/models/administracao/departamento.rb
+++ app/models/administracao/departamento.rb
@@ -3,13 +3,16 @@ class Administracao::Departamento < ActiveRecord::Base
   belongs_to :responsavel,:class_name=>"Pessoa"
   has_one :endereco,:as=>:enderecavel,:dependent=>:destroy
   has_one :rota, :as=>:roteavel,:dependent=>:destroy
+
+
   
 
   attr_accessor :nome_responsavel,:cidade_nome,:bairro_nome
 
   after_create :adicionar_rota
   after_update :editar_rota
-
+  
+  scope :com_responsavel,->{where("responsavel_id > 0")}
 
   accepts_nested_attributes_for :endereco#, :reject_if => proc { |attributes| attributes['logradouro'].blank? },:allow_destroy => true
 
